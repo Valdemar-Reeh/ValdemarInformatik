5ff44ba70ea003056260fa3fb956dcf0154a3715
Valdemar-Reeh <Vald1726@rysensteen.dk>
2025-03-11 11:23:33 +0100

    matriser
    Ny matrise / virker ikke

--- Python/mainMatrics.py
+++ Python/mainMatrics.py
@@ -2,13 +2,13 @@ import numpy as np
 from PIL import Image
 import cv2
 
-image1 = 'image2.jpeg'
-imageTest1 = '20241119-_RVB1178 – lille.jpeg'
-imageTest2 = '20241119-_RVB1178 – mellem.jpeg'
-imageTest3 = '20241119-_RVB1178 – stor.jpeg'
+image1 = 'Billleder/image2.jpeg'
+imageTestLille = 'Billleder/20241119-_RVB1178 – lille.jpeg'
+#imageTestMellem = '20241119-_RVB1178 – mellem.jpeg'
+#imageTestStor = '20241119-_RVB1178 – stor.jpeg'
 
-img = Image.open(imageTest2)
-im = cv2.imread(imageTest2)
+img = Image.open(image1)
+im = cv2.imread(image1)
 img.show()
 pixel_map = img.load()
 
@@ -17,6 +17,8 @@ if img is None or im is None:
     raise ValueError("Image not loaded correctly")
 
 # Define a 5x5 convolution matrix
+# The sum of all elements in the matrix must not be 0
+# Gaussian blur
 convolutionMatrix = np.array([
     [1, 2, 4, 2, 1],
     [2, 4, 8, 4, 2],
@@ -24,6 +26,14 @@ convolutionMatrix = np.array([
     [2, 4, 8, 4, 2],
     [1, 2, 4, 2, 1]
 ])
+# Edge detection
+#convolutionMatrix = np.array([
+#    [-1, -1, 0, 1, 1],
+#    [-1, -1, 0, 1, 1],
+#    [-1, -1, 0, 1, 1],
+#    [-1, -1, 0, 1, 1],
+#    [-1, -1, 0, 1, 1]
+#])
 
 # Loop through the image with adjusted ranges to avoid out-of-bounds errors
 for x in range(im.shape[1]):
@@ -53,11 +63,24 @@ for x in range(im.shape[1]):
                 rTotal.append(r * convolutionMatrix[i][j])
                 gTotal.append(g * convolutionMatrix[i][j])
                 bTotal.append(b * convolutionMatrix[i][j])
-        pixel_map[x, y] = (
+        try:
+            pixel_map[x, y] = (
             int(sum(rTotal) / sum(convolutionMatrix.flatten())), 
             int(sum(gTotal) / sum(convolutionMatrix.flatten())), 
             int(sum(bTotal) / sum(convolutionMatrix.flatten()))
-        )  
+            )
+        except ZeroDivisionError:
+            pixel_map[x, y] = (
+                int(sum(rTotal)),
+                int(sum(gTotal)),
+                int(sum(bTotal))
+            )
+        else:
+            pixel_map[x, y] = (
+                int(sum(rTotal)),
+                int(sum(gTotal)),
+                int(sum(bTotal))
+            )
 img.show()
 
 
